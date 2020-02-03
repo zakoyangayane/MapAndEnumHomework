2030c4975b89a6242a39202c0a6bee1f021772e0
zakoyangayane <gayanezakoyan@gmail.com>
2020-02-03 23:28:35 +0400

    used values() method of enum

--- src/com/homework/year/Main.java
+++ src/com/homework/year/Main.java
@@ -8,25 +8,25 @@ public class Main {
         MonthService monthService = new MonthService();
 
         //print number of holidays in some months
-        System.out.println("July has " + Month.numberOfHolidaysInMonth(monthService.JULY)
+        System.out.println("July has " + Month.numberOfHolidaysInMonth(Month.JULY)
                 + " holidays");
-        System.out.println("November has " + Month.numberOfHolidaysInMonth(monthService.NOVEMBER)
+        System.out.println("November has " + Month.numberOfHolidaysInMonth(Month.NOVEMBER)
                 + " holidays");
 
         //print holidays of a april
-        System.out.println("Holidays of April " + Month.getHolidaysInMonth(monthService.APRIL));
+        System.out.println("Holidays of April " + Month.getHolidaysInMonth(Month.APRIL));
 
         //print number of days in march
-        System.out.println("March has " + Month.getDaysInMonth(monthService.MARCH) + " days");
+        System.out.println("March has " + Month.getDaysInMonth(Month.MARCH) + " days");
 
         //traversing and printing all month names
         System.out.println("Traversed names of months");
         Month.traverseName(monthService.months);
 
         //check whether the current day is a public holiday or not
-        System.out.println(Month.checkWhetherHoliday(monthService.MARCH, 21) ?
+        System.out.println(Month.checkWhetherHoliday(Month.MARCH, 21) ?
                 "March 21 is holiday" : "March 21 isn't a holiday");
-        System.out.println(Month.checkWhetherHoliday(monthService.MARCH, 8) ?
+        System.out.println(Month.checkWhetherHoliday(Month.MARCH, 8) ?
                 "March 8 is a holiday" : "March 8 isn't a holiday");
     }
 }
--- src/com/homework/year/service/MonthService.java
+++ src/com/homework/year/service/MonthService.java
@@ -3,19 +3,5 @@ package com.homework.year.service;
 import com.homework.year.model.Month;
 
 public class MonthService {
-    public final Month JANUARY = Month.JANUARY;
-    public final Month FEBRUARY = Month.February;
-    public final Month MARCH = Month.MARCH;
-    public final Month APRIL = Month.APRIL;
-    public final Month MAY = Month.MAY;
-    public final Month JUNE = Month.JUNE;
-    public final Month JULY = Month.JULY;
-    public final Month AUGUST = Month.AUGUST;
-    public final Month SEPTEMBER = Month.SEPTEMBER;
-    public final Month OCTOBER = Month.OCTOBER;
-    public final Month NOVEMBER = Month.NOVEMBER;
-    public final Month DECEMBER = Month.DECEMBER;
-
-    public final Month[] months = {JANUARY, FEBRUARY, MARCH, APRIL, MAY,
-            JUNE, JULY, AUGUST, SEPTEMBER, OCTOBER, NOVEMBER, DECEMBER};
+    public final Month[] months = Month.values();
 }
